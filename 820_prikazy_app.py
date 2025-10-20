f70d89aae4a9e8a9d09b04a44b7c07844a6017ab
Theodor Kurtz <vysusil.theodor@gmail.com>
2025-10-20 09:53:40 +0200

    Add files via upload

--- 820_prikazy_app.py
+++ 820_prikazy_app.py
@@ -1,72 +1,106 @@
 import streamlit as st
 
-# hodnoty vstupu
-A1 = "IO1 digitální vstup IO1"
-A2 = "IO1 přepínač S/S"
-A3 = "IO1 analogový vstup IO1"
-A4 = "IO1 5V zdroj (trvale)"
-A5 = "IO1 5V zdroj (při +15)"
-A6 = "IO1 in - OPER - nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu na základě sepnutí IO1)"
-A7 = "IO1 out - OPER - nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu a sepnutí IO1)"
-A8 = "IO1 výstup IO1"
-A9 = "IO1 AN - IBR"
-B1 = "IO2 přepínač S/S"
-B2 = "IO2 dig. vstup IO2"
-B3 = "IO2 analogový vstup IO2"
-B4 = "IO2 výstup IO2"
-B5 = "IO2 in - OPER -nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu na základě sepnutí IO1)"
-B6 = "IO2 AN - IBR"
-C1 = "IO3 dig. vstup IO3"
-C2 = "IO3 přepínač S/S"
-C3 = "IO3 váhy RVS"
-C4 = "IO3 výstup IO3"
-C5 = "IO3 RFID"
-D1 = "IO4 +15 drátová"
-D2 = "IO4 dig. vstup IO4"
-D3 = "IO4 přepínač S/S"
-E1 = "IO5 RFID"
-E2 = "IO5 dig. vstup IO5"
-E3 = "IO5 přepínač S/S"
-E4 = "IO5 výstup IO5"
-E5 = "IO5 Dallas - při použití teploměrů je nutné nastavit IO1 jako 5V zdroj pro jejich napájení"
-F1 = "CAN1 FMS rychlost 250"
-F2 = "CAN1 FMS rychlost 500"
-F3 = "CAN1 Full FMS rychlost 250"
-F4 = "CAN1 Full FMS rychlost 500"
-F5 = "CAN1 tachograf rychlost 250"
-F6 = "CAN1 tachograf rychlost 500"
-F7 = "CAN1 IBR, pouze čtení zprávy o celkovém stavu nádrže"
-F8 = "CAN1 zakázání"
-G1 = "CAN2 FMS rychlost 250"
-G2 = "CAN2 FMS rychlost 500"
-G3 = "CAN2 Full FMS rychlost 250"
-G4 = "CAN2 Full FMS rychlost 500"
-G5 = "CAN2 tachograf rychlost 250"
-G6 = "CAN2 tachograf rychlost 500"
-G7 = "CAN2 IBR, pouze čtení zprávy o celkovém stavu nádrže"
-G8 = "CAN2 zakázání"
-H1 = "jizda dratova +15 + Acc + Nap"
-H2 = "jizda dratova +15 + Acc"
-H3 = "jizda dratova Acc + Nap"
-H4 = "jízda ACC"
-H5 = "jízda +15"
-H6 = "jízda Nap"
-I1 = "roaming"
+# --- Hodnoty vstupu (stejné jako předtím) ---
+A1 = "dig. vstup IO1"
+A2 = "přep. S/S IO1"
+A3 = "anal. vstup IO1"
+A4 = "5V zdroj (trvale) IO1"
+A5 = "5V zdroj (při +15) IO1"
+A6 = "in - OPER - nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu na základě sepnutí IO1) IO1"
+A7 = "out - OPER - nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu a sepnutí IO1) IO1"
+A8 = "výstup IO1"
+A9 = "AN - IBR IO1"
 
-# Možnosti pro jednotlivé vstupy
+B1 = "přep. S/S IO2"
+B2 = "dig. vstup IO2"
+B3 = "anal. vstup IO2"
+B4 = "výstup IO2"
+B5 = "in - OPER -nastavení pro APEX (generování poplachů dle SASS/GAS do OPERu na základě sepnutí IO1) IO2"
+B6 = "AN - IB IO2"
+
+C1 = "dig. vstup IO3"
+C2 = "přep. S/S IO3"
+C3 = "váhy RVS IO3"
+C4 = "výstup IO3"
+C5 = "RFID IO3"
+
+D1 = "+15 drátová IO4"
+D2 = "dig. vstup IO4"
+D3 = "přepínač S/S IO4"
+
+E1 = "RFID IO5"
+E2 = "dig. vstup IO5"
+E3 = "přep. S/S IO5"
+E4 = "výstup IO5"
+E5 = "teploměry Dallas IO5- při použití teploměrů je nutné nastavit IO1 jako 5V zdroj pro jejich napájení"
+
+F1 = "FMS 250 CAN1"
+F2 = "FMS 500 CAN1"
+F3 = "Full FMS 250 CAN1"
+F4 = "Full FMS 500 CAN1"
+F5 = "tachograf 250 CAN1"
+F6 = "tachograf 500 CAN1"
+F7 = "IBR, pouze čtení zprávy o celkovém stavu nádrže CAN1"
+F8 = "zakázání CAN1"
+
+G1 = "FMS 250 CAN2"
+G2 = "FMS 500 CAN2"
+G3 = "Full FMS 250 CAN2"
+G4 = "Full FMS 500 CAN2"
+G5 = "tachograf 250 CAN2"
+G6 = "tachograf 500 CAN2"
+G7 = "IBR, pouze čtení zprávy o celkovém stavu nádrže CAN2"
+G8 = "zakázání CAN2"
+
+H1 = "dotaz na vstupy"
+H2 = "smazání nastavení všech IO"
+H3 = "nastavení výrobní konfigurace IO"
+
+I1 = "jizda +15, Acc, Nap"
+I2 = "jizda +15, Acc"
+I3 = "jizda Acc, Nap"
+I4 = "jízda ACC"
+I5 = "jízda +15"
+I6 = "jízda Nap"
+
+J1 = "nastavení roamingu"
+J2 = "deaktivace roamingu"
+J3 = "ověření nastavení roamingu"
+
+K1 = "dotaz na GPS ant"
+K2 = "auto detekce GPS ant"
+K3 = "interní GPS ant"
+K4 = "extrerní GPS antu"
+
+Z1 = "dotaz na verzi"
+Z2 = "dotaz na verzi CAN knihovny"
+Z3 = "reset"
+Z4 = "kompletni vymazani dat"
+Z5 = "výpis konfigurace"
+
+
+# --- Možnosti pro jednotlivé vstupy ---
 moznosti = {
+    "CAN2 L": [G1, G2, G3, G4, G5, G6, G7, G8],
+    "CAN1 L": [F1, F2, F3, F4, F5, F6, F7, F8],
     "IO1": [A1, A2, A3, A4, A5, A6, A7, A8, A9],
-    "IO2": [B1, B2, B3, B4, B5, B6],
+    "+AKU": [],
     "IO3": [C1, C2, C3, C4, C5],
+    "+30": [],
+    "CAN2 H": [G1, G2, G3, G4, G5, G6, G7, G8],
+    "CAN1 H": [F1, F2, F3, F4, F5, F6, F7, F8],
+    "IO2": [B1, B2, B3, B4, B5, B6],
     "IO4": [D1, D2, D3],
     "IO5": [E1, E2, E3, E4, E5],
-    "CAN1": [F1, F2, F3, F4, F5, F6, F7, F8],
-    "CAN2": [G1, G2, G3, G4, G5, G6, G7, G8],
-    "Spínání od": [H1, H2, H3, H4, H5, H6],
-    "Ostatní": [I1]
+    "-30": [],
+    "vstupy": [H1, H2, H3],
+    "spínání od": [I1, I2, I3, I4, I5, I6],
+    "roaming": [J1, J2, J3],
+    "GPS anténa": [K1, K2, K3, K4],
+    "ostatní": [Z1, Z2, Z3, Z4, Z5],
 }
 
-# Texty
+# --- Texty (jen pár pro ukázku, u tebe zůstane celý slovník) ---
 texty = {
     A1: "iop(d,{0,1},1);iop(a, 0,{0},1);",
     A2: "iop(d,{0,1},1);iop(a, 5,{0},1);",
@@ -77,25 +111,30 @@ texty = {
     A7: "iop(d,{0,1},1);iop(a, 23,{1},1);",
     A8: "iop(d,{0,1},1);iop(a, 3,{1},1);",
     A9: "iop(d,{0,1},1);iop(a, 17,{1},1);anp(at,1,5000,5,60);IOR=9;IOF=22;SASS={{0x10000,{0}},{0,{2,3}}};DSM=2;IOR;IOF;SASS;DSM;",
+
     B1: "iop(d,{2,3},1);iop(a, 5,{2},1);",
     B2: "iop(d,{2,3},1);iop(a, 0,{2},1);",
     B3: "iop(d,{2,3},1);iop(a, 17,{3},1);",
     B4: "iop(d,{2,3},1);iop(a, 3,{3},1);",
     B5: "iop(d,{2,3},1);iop(a, 3,{3},1);",
     B6: 'iop(d,{2,3},1);iop(a, 17,{3},1);anp(at,3,5000,5,60);IOR=10;IOF=21;SEB={"1991010","","","","",""};SASS={{0x20000,{0}},{0,{2,3}}};CANDEV=4;CANDEV;',
+   
     C1: "iop(d,{4,5},1);iop(a, 0,{4},1);",
     C2: "iop(d,{4,5},1);iop(a, 5,{4},1);",
     C3: "iop(d,{4,5,8,9},1);iop(a, 25,{4,9},1);",
     C4: "iop(d,{4,5},1);iop(a, 3,{5},1);",
     C5: "iop(d,{4,5,8,9},1);iop(a, 10,{4},1);",
+
     D1: "iop(d,{6,7},1);iop(a, 4,{6},1);",
     D2: "iop(d,{6,7},1);iop(a, 0,{6},1);",
     D3: "iop(d,{4,5,8,9},1);iop(a, 10,{4},1);",
+
     E1: "iop(d,{8,9},1);iop(a, 10,{8},1);",
     E2: "iop(d,{8,9},1);iop(a, 0,{8},1);",
     E3: "iop(d,{8,9},1);iop(a, 5,{8},1);",
     E4: "iop(d,{8,9},1);iop(a, 3,{9},1);",
     E5: "iop(d,{8,9},1);iop(a, 11,{8,9},1);",
+
     F1: "CAN1={0,0,120};",
     F2: "CAN1={0,32,120};",
     F3: "CAN1={0,1,120};",
@@ -104,6 +143,7 @@ texty = {
     F6: "CAN1={0,40,120};",
     F7: "CAN1={0,16,120};",
     F8: "CAN1={-1,0,120};",
+
     G1: "CAN2={0,0,120};",
     G2: "CAN2={0,32,120};",
     G3: "CAN2={0,1,120};",
@@ -112,39 +152,81 @@ texty = {
     G6: "CAN2={0,40,120};",
     G7: "CAN2={0,16,120};",
     G8: "CAN2={-1,0,120};",
-    H1: "DSM=15;",
-    H2: "DSM=3;",
-    H3: "DSM=13;",
-    H4: "DSM=1;",
-    H5: "DSM=2;",
-    H6: "DSM=8;",
-    I1: "GRM=2;"
+        
+    H1: "iop(p);",
+    H2: "iop(d,{0,1,2,3,4,5,6,7,8,9},1);",
+    H3: "iop(d,{0,1,2,3,4,5,6,7,8,9},1);iop(a,0,{0,4},1);iop(a,5,{2},1);iop(a,4,{6},1);iop(a,10,{8},1);iop(p);",
+
+    I1: "DSM=15;",
+    I2: "DSM=3;",
+    I3: "DSM=13;",
+    I4: "DSM=1;",
+    I5: "DSM=2;",
+    I6: "DSM=8;",
+    
+    J1: "GRM=2;",
+    J2: "GRM=0;",
+    J3: "GRM;",
+    
+    K1: "GPSANT;",
+    K2: "GPSANT=0;",
+    K3: "GPSANT=2;",
+    K4: "GPSANT=1;",
+    
+    Z1: "ver();",
+    Z2: "ver(can);",
+    Z3: "reset();",
+    Z4: "unit(spoo,f);",
+    Z5: "pp();",
 }
 
-# --- Inverzní slovník (text -> popis) ---
 inverse_texty = {v: k for k, v in texty.items()}
 
-st.markdown("<p style='font-size:20px;'>Výběr možností nastavení</p>", unsafe_allow_html=True)
+# --- Rozložení rozhraní ---
+st.markdown("### ⚙️ Konfigurace IO / CAN rozhraní")
+st.markdown("#### Rozložení svorek podobně jako panel jednotky:")
 
-# Výběr možností přes selectboxy
+# --- První řádek (CAN2 L, CAN1 L, IO1, +AKU, IO3, +30) ---
+cols_top = st.columns([5, 5, 5, 2, 5, 2])
+labels_top = ["CAN2 L", "CAN1 L", "IO1", "+AKU", "IO3", "+30"]
+
 vybrane_texty = []
-for vstup, moznosti_list in moznosti.items():
-    vybrano = st.selectbox(f"{vstup}:", [""] + moznosti_list, key=vstup)
-    if vybrano:
-        vybrane_texty.append(texty[vybrano])
+for i, label in enumerate(labels_top):
+    with cols_top[i]:
+        vybrano = st.selectbox(label, [""] + moznosti[label], key=label)
+        if vybrano:
+            vybrane_texty.append(texty.get(vybrano, ""))
 
-# Zobrazení dopředného výstupu
+# --- Druhý řádek (CAN2 H, CAN1 H, IO2, IO4, IO5, -30) ---
+cols_bottom = st.columns([5, 5, 5, 5, 5, 2])
+labels_bottom = ["CAN2 H", "CAN1 H", "IO2", "IO4", "IO5", "-30"]
+
+for i, label in enumerate(labels_bottom):
+    with cols_bottom[i]:
+        vybrano = st.selectbox(label, [""] + moznosti[label], key=label)
+        if vybrano:
+            vybrane_texty.append(texty.get(vybrano, ""))
+
+# --- Třetí řádek (spínání od, roaming, GPS anténa, ostatní) ---
+cols_down = st.columns([5, 5, 5, 5, 5])
+labels_down = ["vstupy", "spínání od", "roaming", "GPS anténa", "ostatní"]
+
+for i, label in enumerate(labels_down):
+    with cols_down[i]:
+        vybrano = st.selectbox(label, [""] + moznosti[label], key=label)
+        if vybrano:
+            vybrane_texty.append(texty.get(vybrano, ""))
+
+# --- Výstupní textové pole ---
 vystup = "".join(vybrane_texty)
-input_text = st.text_area("NKP příkaz", value=vystup, height=250)
+st.text_area("NKP příkaz", value=vystup, height=200)
 
-# --- Opačný směr: zjistit, co odpovídá vloženému textu ---
+# --- Inverzní hledání (opačný směr) ---
+input_text = st.text_area("Zadej NKP příkaz pro rozpoznání:", height=150)
 if input_text:
-    nalezene = []
-    for kod, popis in inverse_texty.items():
-        if kod in input_text:
-            nalezene.append(f"{popis}")
+    nalezene = [popis for kod, popis in inverse_texty.items() if kod in input_text]
     if nalezene:
-        st.write("Vložený NKP příkaz je nastavení pro")
+        st.success("Vložený NKP příkaz odpovídá nastavením:")
         st.write(", ".join(nalezene))
     else:
-        st.write("Žádná shoda nenalezena.")
+        st.warning("Žádná shoda nenalezena.")
